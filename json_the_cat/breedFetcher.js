394981512a5df289cb52890ce000cbdca5e36c6e
Andrei Chirita <adchir95@gmail.com>
2022-09-29 20:47:01 +0000

    updating to appropriate folder

---
+++ json_the_cat/breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const args = process.argv.slice(2);
+
+const breedDescription = fetchBreedDescription('Siberian', (error, description) => {
+
+});
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  } else if (Boolean(!response.length)) {
+    return console.log('Cat breed not found');
+  } else {
+    const data = JSON.parse(body);
+    console.log(data[0].description);
+    console.log(typeof data);
+  }
+});
